fd59d3486c68403d05e9836070a42c3cf91a5e8e
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-08-19 20:17:48 -0400

    First commit.

--- connection002/src/Main.java
+++ connection002/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+
         DatabaseFunctions myDatabase = new DatabaseFunctions();
 
         myDatabase.coneta("postgres","postgres","1234567");
